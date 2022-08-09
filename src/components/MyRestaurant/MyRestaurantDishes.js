200d579eee7e3ac8f87dcf752875fee1705ee144
MayraMendoza <mayra601m@gmail.com>
2022-08-08 21:30:01 -0400

    added my restaurant

---
+++ src/components/MyRestaurant/MyRestaurantDishes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../common/Container";
+
+const MyRestaurantDishes =() =>{
+    return(
+        <Container>
+            <h1>My Restaurant Dishes</h1>
+
+        </Container>
+    )
+}
+export default MyRestaurantDishes;
